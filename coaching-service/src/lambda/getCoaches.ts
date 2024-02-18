942a136503aab16467d6ed60fd7c83f35d285ca9
alinili414e <alinili414e@gmail.com>
2024-02-18 14:35:43 -0500

    getCoaches added

---
+++ coaching-service/src/lambda/getCoaches.ts
@@ -0,0 +1,54 @@
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+
+const dynamoDbClient = new DynamoDBClient({ region: "us-east-1" });
+
+exports.handler = async (event) => {
+  const tableName = "coachInfoTable";
+
+  try {
+    const scanCommand = new ScanCommand({
+      TableName: tableName,
+    });
+    const response = await dynamoDbClient.send(scanCommand);
+
+    // Check if Items is defined before proceeding
+    if (!response.Items) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "No coaches found",
+          data: [],
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+    }
+
+    const coaches = response.Items.map((item) => unmarshall(item));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Successfully retrieved coaches",
+        data: coaches,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data from DynamoDB", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Failed to retrieve coaches",
+        error: error.message,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
+};
